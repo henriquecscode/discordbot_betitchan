9cd771f8d4b5efda22faac839df508296b20cfda
henriquecscode <hcs_games@hotmail.com>
2018-08-08 14:22:16 +0100

    Made your changes work

--- bot3.js
+++ bot3.js
@@ -7,26 +7,20 @@ var prefixlenght = prefix.length
 var commands = [
 
     [
-    "Help",
-    `${sender} Oni-Chan, you can use these commands :3
-    **Ping** It will send to you the Pong~
-    **Stromi** 2Bee is the best!
-    **Neko** Sends a neko nyan~
-    **Devaster** Sends a Devaster Emoji, more coming soon!~~
-    These are all the commands that you can use for now~`
-    ],
-    [
     "Ping",
+    "It will send to you the Pong",
     "pong"
     ],
 
     [
     "Stromi",
+    "2Bee is the best!",
     "2Bee is cool"
     ],
 
     [
     "Neko",
+    "Sends a neko nyan~,
     "https://cdn.discordapp.com/attachments/476134769160355871/476384779235885097/latest.png",
     "https://cdn.discordapp.com/attachments/476134769160355871/476384816393093130/main-qimg-d658e310e79b5f5218c84efa57b4ca37.png",
     "https://cdn.discordapp.com/attachments/476134769160355871/476384857543540736/images.png",
@@ -37,6 +31,7 @@ var commands = [
     
     [
     "Devaster",
+    "Sends a Devaster Emoji, more coming soon!",
     "https://cdn.discordapp.com/attachments/476111531512299521/476399145817473048/DAngry.png",
     "https://cdn.discordapp.com/attachments/476111531512299521/476399147390337058/DCool.png",
     "https://cdn.discordapp.com/attachments/476111531512299521/476399148652822528/DGun.png",
@@ -52,6 +47,7 @@ var commands = [
 ];
 //2d array that contains the functionality of each function
 //Each row's first element contains the string with the name of the command
+//Each ro'w second element contains what is going to be display by the help command
 //The other elements of the row are the possile answers
 //Edit this to add more commands
 
@@ -68,7 +64,7 @@ function Chansmessages(usermessage)
     for(var i = 0; i < commands.length;i++) //Runs through all commands' elements to see if the message coincides with any function of the bot
     {
         if(usermessage === commands[i][0].toLowerCase()){ //Checks if the command is the same as in the commands[i][0] array
-            index = Math.floor(Math.random() * (commands[i].length-1) + 1); //Randomizes an index between the 2nd element and the last
+            index = Math.floor(Math.random() * (commands[i].length-2) + 2); //Randomizes an index of the available response options
             return commands[i][index]; //Returns the correspondent string to that index
         }
     }
@@ -225,7 +221,7 @@ client.on("message", (message) => { //When there is a message in the server, get
             message.channel.send(`${sender} Here is a list of what you can do`);
             for(var i = 0; i < commands.length; i++)
             {
-                allcommandsstring += "**" + commands[i][0] + "**" + '\n'; //Makes so each new command is in bold and is succeeded by a new line
+                allcommandsstring += "**" + commands[i][0] + "**" + " ~" + commands[i][1] + "~" + '\n'; //Makes so each new command is in bold, followed by its description and is succeeded by a new line
             }
             message.channel.send(allcommandsstring);
             //Outputs the multiple commands: the first element of each row of the commands 2d array
