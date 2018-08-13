c2b4b4b53000c431cafc0b1331552f7afb72dd68
henriquecscode <hcs_games@hotmail.com>
2018-08-13 19:39:30 +0100

    Edited the code so there is a server object that holds a queue, as it is in the videos tutorials

--- music.js
+++ music.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const YTDL = require ("ytdl-core"); //Youtube Download - Used to download the youtube link and stream it
 
-var queue = []; //Stores the links of the musics to be played
+var server = {queue: []}; //Stores the links of the musics to be played
 
 exports.MusicPlay = function (message, link){
     if(!link){ //The user did not specify a link
@@ -12,7 +12,7 @@ exports.MusicPlay = function (message, link){
         return message.channel.send(`${message.member} Please join a voice channel`);
     }
 
-    queue.push(link);
+    server.queue.push(link); //Adds the link to the queuekk
 
     if(!message.guild.voiceConnection){ //If we are not connected to the voice channel
         message.member.voiceChannel.join() //Async join the voice channel function
@@ -24,7 +24,7 @@ exports.MusicPlay = function (message, link){
 
 exports.MusicSkip = function(){
     if (queue.dispatcher){ //If there is a music playing
-        queue.dispatcher.end(); //End the music
+        server.dispatcher.end(); //End the music
     }
 }
 
@@ -36,12 +36,14 @@ exports.MusicStop = function(message){
 
 function MusicQueue(connection){
     console.log("It reached so far");
-    queue.dispatcher = connection.playStream(YTDL(queue[0], {filter: "audioonly"})); //Strema the music, using YTDL to download the queue[0] link, though only its audio (filter: "audioonly")
-    console.log("And also kept going");
-    queue.shift(); //Removes the first link of the queue 
+    console.log(server.queue[0]);
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"})); //Stream the music, using YTDL to download the queue[0] link, though only its audio (filter: "audioonly")
 
-    queue.dispatcher.on("end", function(){
-        if(queue[0]){ //If there is still a music in the queue after having played one
+    console.log("And also kept going");
+    server.queue.shift(); //Removes the first link of the queue 
+
+    server.dispatcher.on("end", function(){
+        if(server.queue[0]){ //If there is still a music in the queue after having played one
             MusicQueue(connection) //Keeps playing music by calling itself
         }
         else{
